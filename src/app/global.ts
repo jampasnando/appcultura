811c53661fb8b2a4efc0da726ae09b3e90104b3e
jampasnando <jampasnando@gmail.com>
2019-07-04 17:47:16 -0400

    con titulo de la app

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,6 @@
 export const GLOBAL={
     imgs:"http://192.168.220.106/municipio/public/images/agenda_cultural/",
     servicios:"http://192.168.220.106/municipio/public/cultura/"
-
+    // imgs:"http://10.42.0.117/municipio/public/images/agenda_cultural/",
+    // servicios:"http://10.42.0.117/municipio/public/cultura/"
 }
